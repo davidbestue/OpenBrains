fe22e02271c0c3aa6254e22adee9fd61bc4111ad
David Bestue <davidsanchezbestue@hotmail.com>
2022-09-29 18:23:28 +0200

    update

--- Implicit_bias_pavlovia/task/openIAT.py
+++ Implicit_bias_pavlovia/task/openIAT.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.2.3),
-    on septiembre 29, 2022, at 18:18
+    on septiembre 29, 2022, at 18:23
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
