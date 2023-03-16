add22a758cc848bd5df5f8c974889c234d3e0094
David Bestue <davidsanchezbestue@hotmail.com>
2023-03-16 19:39:51 +0100

    update

--- Stroop/stroop_task_lastrun.py
+++ Stroop/stroop_task_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.2.3),
-    on marzo 08, 2023, at 17:45
+    on marzo 09, 2023, at 19:33
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
