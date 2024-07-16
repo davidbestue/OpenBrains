935c8efe81e81e814d19e69dfe46457f1bb9f20e
David Bestue <davidsanchezbestue@hotmail.com>
2024-07-16 11:45:01 +0200

    update

--- coche_autonomo_pavlovia/task/tarea_coche_autonomo_lastrun.py
+++ coche_autonomo_pavlovia/task/tarea_coche_autonomo_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2023.2.3),
-    on July 16, 2024, at 11:21
+    on July 16, 2024, at 11:34
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
