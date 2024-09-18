bb8a6726655ec48f2099ea25fe307ac785a0b5e3
David Bestue <davidsanchezbestue@hotmail.com>
2024-09-18 18:42:59 +0200

    update

--- iceberg/task/tarea_iceberg_lastrun.py
+++ iceberg/task/tarea_iceberg_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2023.2.3),
-    on September 18, 2024, at 17:49
+    on September 18, 2024, at 17:56
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
